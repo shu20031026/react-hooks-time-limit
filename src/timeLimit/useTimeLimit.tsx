ea8c83d48bc11c39d3c1c624403b86e7dc059bbe
shu20031026 <shu20031026@gmail.com>
2021-12-28 21:15:02 +0900

    update useTimeLimit

--- src/timeLimit/useTimeLimit.tsx
+++ src/timeLimit/useTimeLimit.tsx
@@ -4,24 +4,24 @@ import { TimeLimitOption } from "./type"
 const getTimeLeft = (targetDate:Date , isSec: boolean) => {
   return (
     isSec ? 
-    Math.floor((targetDate.getTime() - Date.now())/1000) :
-    (targetDate.getTime() - Date.now())
+      Math.floor((targetDate.getTime() - Date.now())/1000) :
+      (targetDate.getTime() - Date.now())
   )
 }
 
 export const useTimeLimit = (
-  targetDate:Date, 
+  targetDate: Date, 
   { intervalTime = 1000, isSec = false }:TimeLimitOption = {}
-) => {
+) :[number, React.Dispatch<React.SetStateAction<Date>>] => {
   const [timeLeft, setTimeLeft] = useState<number>(getTimeLeft(targetDate, isSec))
   const [targetTime, setTargetTime] = useState<Date>(targetDate)
 
   useEffect(() => {
-    const id = setInterval(() => {
+    const intervalUpdate = setInterval(() => {
       setTimeLeft(getTimeLeft(targetTime,isSec))
     }, intervalTime)
     return () => {
-      clearInterval(id)
+      clearInterval(intervalUpdate)
     }
   }, [])
 
